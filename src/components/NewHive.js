3317fe809e4ac30d6088e058b3767ea7c77b8e88
Ben Hur Martins <Ben_Hur@MacBook-Air-de-Ben.local>
2018-10-20 05:12:34 -0300

    map settings 2

--- src/components/NewHive.js
+++ src/components/NewHive.js
@@ -1,12 +1,15 @@
 import React from "react";
-import { StyleSheet, Text, View, Platform } from "react-native";
+import { StyleSheet, Text, View, Platform, Image } from "react-native";
 import { MapView, Constants, Location, Permissions } from "expo";
+import { Button, Fab } from "native-base";
+import { Icon } from "react-native-elements";
 
 export default class NewHive extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      active: "false",
       region: {
         latitude: 37.78825,
         longitude: -122.4324,
@@ -34,17 +37,14 @@ export default class NewHive extends React.Component {
     var region = {
       latitude: location.coords.latitude,
       longitude: location.coords.longitude,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421
+      latitudeDelta: 0.0461,
+      longitudeDelta: 0.0211
     };
 
     var myPosition = {
       latitude: location.coords.latitude,
       longitude: location.coords.longitude
     };
-
-    console.log("imprimindo");
-    console.log(MapView.Marker);
 
     this.setState({ region, myPosition });
   };
@@ -62,29 +62,54 @@ export default class NewHive extends React.Component {
 
   render() {
     return (
-      <MapView
-        provider={MapView.PROVIDER_GOOGLE}
-        style={styles.map}
-        region={this.state.region}
-        // onRegionChange={region => this.onRegionChange(region)}
-      >
-        <MapView.Marker
-          coordinate={this.state.myPosition}
-          title={"To aqui"}
-          description={"To Aqui mesmo"}
-        />
-        <Text>Teste</Text>
-      </MapView>
+      <View style={styles.container}>
+        <MapView
+          mapType={"satellite"}
+          provider={MapView.PROVIDER_GOOGLE}
+          style={styles.map}
+          region={this.state.region}
+          // onRegionChange={region => this.onRegionChange(region)}
+        >
+          <MapView.Marker
+            coordinate={this.state.myPosition}
+            title={"To aqui"}
+            description={"To Aqui mesmo"}
+          >
+            <Image
+              style={{ width: 35, height: 55 }}
+              source={require("../assets/my-position-marker.png")}
+            />
+          </MapView.Marker>
+        </MapView>
+        <Fab
+          active={this.state.active}
+          direction="up"
+          containerStyle={{}}
+          style={{ backgroundColor: "#FBE312" }}
+          position="bottomRight"
+          onPress={() => this.setState({ active: !this.state.active })}
+        >
+          <Icon name="add" color={"#4A4A4A"} />
+          <Button
+            style={{ backgroundColor: "transparent" }}
+            onPress={() => console.log("teste")}
+          >
+            <Image
+              style={{ width: 35, height: 35 }}
+              source={require("../assets/hive-map-icon.png")}
+            />
+          </Button>
+        </Fab>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    // flex: 1,
-    // backgroundColor: "#fff",
-    // alignItems: "center",
-    // justifyContent: "center"
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: "flex-end",
+    alignItems: "center"
   },
   map: {
     ...StyleSheet.absoluteFillObject
